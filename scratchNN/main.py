b0b2ba46205c65798c1362f501f79f991de20bc7
netotz <neto.otz@hotmail.com>
2019-08-06 22:56:23 -0500

    add training inputs

--- scratchNN/main.py
+++ scratchNN/main.py
@@ -11,12 +11,15 @@ print(neural_network.synaptic_weights)
 training_inputs = np.array([[0, 0, 1],
                             [1, 1, 1],
                             [1, 0, 1],
-                            [0, 1, 1]])
-training_outputs = np.array([[0, 1, 1, 0]]).T
+                            [0, 1, 1],
+                            [0, 0, 0],
+                            [1, 1, 0]])
+# corresponding outputs
+training_outputs = np.array([[0, 1, 1, 0, 0, 1]]).T
 
 print("The network is training...")
 # arbitrary number of iterations
-neural_network.train(training_inputs, training_outputs, 15000)
+neural_network.train(training_inputs, training_outputs, 10000)
 
 print("Adjusted weights after training:")
 print(neural_network.synaptic_weights)
@@ -25,8 +28,9 @@ while True:
     print("Introduce a new data to process (e.g. '1 0 0'): ", end='')
     try:
         new_data = input().split()
+        result = neural_network.think(np.array(new_data))
     except:
         break
-    result = neural_network.think(np.array(new_data))
+    
     print("My best forecast is ", result)
     print()
