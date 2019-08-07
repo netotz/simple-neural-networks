bfaa68884c8852af3699554a1720f08f42ad0328
netotz <neto.otz@hotmail.com>
2019-08-06 22:35:01 -0500

    add line that prints the result

--- scratchNN/main.py
+++ scratchNN/main.py
@@ -1,4 +1,4 @@
-import scratchNN.NeuralNetwork as NN
+import NeuralNetwork as NN
 import numpy as np
 
 neural_network = NN.NeuralNetwork()
@@ -8,11 +8,11 @@ print(neural_network.synaptic_weights)
 
 # training data:
 # 4 examples 
-training_inputs = np.array([[0,0,1],
-                            [1,1,1],
-                            [1,0,1],
-                            [0,1,1]])
-training_outputs = np.array([[0,1,1,0]]).T
+training_inputs = np.array([[0, 0, 1],
+                            [1, 1, 1],
+                            [1, 0, 1],
+                            [0, 1, 1]])
+training_outputs = np.array([[0, 1, 1, 0]]).T
 
 print("The network is training...")
 # arbitrary number of iterations
@@ -24,4 +24,4 @@ print(neural_network.synaptic_weights)
 print("Introduce a new data to process (e.g. '1 0 0'): ", end='')
 new_data = input().split()
 result = neural_network.think(np.array(new_data))
-print("My best forecast is ", )
+print("My best forecast is ", result)
